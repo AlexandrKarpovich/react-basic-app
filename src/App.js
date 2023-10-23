1e72ec1027be0afcc9f9ff4f272556fd7b8bff92
alex karpovich <alieksandr.karpovich@mail.ru>
2023-10-24 01:43:18 +0300

    hook useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { useState } from "react";
+
 function App() {
+  const [likes, setLikes] = useState(0)
+  const [value, setValue] = useState('fdfd')
+  console.log(value)
+  console.log(setValue)
+  
+  // let likes = 0;
   return (
-    <h2>Basic React</h2>
+    <>
+      <div style={{fontSize: 30}}>{likes}</div>
+      <button onClick={ () => setLikes(likes + 1) }>Click +</button>
+      <button onClick={ () => setLikes(likes - 1) }>Click -</button>
+
+      <h3>{value}</h3>
+      <input 
+        type='text' 
+        value={value}
+        onChange={e => setValue(e.target.value)} />
+    </>
   );
 }
 
